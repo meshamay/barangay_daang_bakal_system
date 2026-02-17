7597984b65bb22d7ba821216a2777f1631950eed
Mesha <bmeshamay@barcebas>
2026-02-17 13:33:49 +0800

    Add super admin account to UserSeeder for team access

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -10,8 +10,29 @@ class UserSeeder extends Seeder
 {
     public function run(): void
     {
+        // Create Super Admin account
+        User::create([
+            'resident_id' => 'SA-00001',
+            'first_name' => 'Super',
+            'last_name' => 'Admin',
+            'username' => 'superadmin',
+            'email' => 'superadmin@daangbakal.gov',
+            'password' => Hash::make('SuperAdmin@2026'),
+            'plain_password' => 'SuperAdmin@2026',
+            'user_type' => 'super admin',
+            'role' => 'super admin',
+            'status' => 'approved',
+            'gender' => 'Male',
+            'age' => 35,
+            'civil_status' => 'Single',
+            'birthdate' => '1991-01-01',
+            'place_of_birth' => 'Manila',
+            'citizenship' => 'Filipino',
+            'contact_number' => '09171234567',
+            'address' => 'Barangay Daang Bakal Office',
+        ]);
+
+        // Create regular users
         User::factory()->count(10)->create();
-
-
     }
 }
